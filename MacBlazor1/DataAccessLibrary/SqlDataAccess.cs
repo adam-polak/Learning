7f9dc187970b6965c5015f2b2b2d3a71b8c50947
Adam Polak <adamjpolak@gmail.com>
2024-05-19 14:21:49 -0500

    setup DataAccessLibrary

---
+++ MacBlazor1/DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,28 @@
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLibrary {
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config) {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters) {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using(IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+                return data.ToList();
+            }
+        }
+
+    }
+}
