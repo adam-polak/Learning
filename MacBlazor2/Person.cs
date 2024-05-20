de1cf97fb97407140b93a75be17a24fadc5e1620
Adam Polak <adamjpolak@gmail.com>
2024-05-20 11:01:52 -0500

    made person model

---
+++ MacBlazor2/Person.cs
@@ -0,0 +1,9 @@
+namespace MacBlazor2
+{
+    public class Person
+    {
+        public int actor_id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+}
