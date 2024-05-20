ce67a133976fb9abec85c5f8b60fe5161c9d3117
Adam Polak <adamjpolak@gmail.com>
2024-05-20 11:16:02 -0500

    setting up helper class

---
+++ MacBlazor2/Helper.cs
@@ -0,0 +1,12 @@
+using System.Configuration;
+
+namespace MacBlazor2
+{
+    public static class Helper
+    {
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
