8b5e592172a04645193d89edc95e1ec761e818c7
Adam Polak <adamjpolak@gmail.com>
2024-05-30 22:03:38 -0500

    starting dvdrental db display for practice

--- MacBlazor2/DbHelper.cs
+++ MacBlazor2/DbHelper.cs
@@ -10,7 +10,6 @@ namespace MacBlazor2
 
         public DbHelper() {
             connection = new NpgsqlConnection("Host=localhost:5432;Username=postgres;Password=password;Database=dvdrental");
-            Console.WriteLine(connection.Database);
             Console.WriteLine(connection.ConnectionString);
             foreach(Person p in connection.Query<Person>("SELECT actor_id, first_name, last_name FROM actor")) Console.WriteLine(p.first_name + " " + p.last_name);
         }
