93a589e274ab875aefefae0b108814c568ebba41
Adam Polak <adamjpolak@gmail.com>
2024-06-13 18:53:33 -0500

    make new class print info

--- Flashcards/UILogic/Menu.cs
+++ Flashcards/UILogic/Menu.cs
@@ -4,47 +4,69 @@ namespace UILogic;
 
 class Menu
 {
+
     Panel p;
-    private static List<string> MenuTypes = ["Main", "Add", "Delete", "View Set", "Practice", "View Scores"];
+    private static List<string> pickMenuList = ["Pick", "Back"];
     private static Dictionary<string, List<string>> commands = new Dictionary<string, List<string>>() 
     {
         { "Main", ["Add", "Delete", "View Set", "Practice", "View Scores"] },
         { "Add", ["Add Set", "Add Element To Set", "Back"] },
-        { "Add Element To Set", ["Pick", "Back"] },
+        { "Add Set", pickMenuList },
+        { "Add Element To Set", pickMenuList },
         { "Delete", ["Delete Set", "Delete Element From Set", "Back"] },
-        { "Delete Element From Set", ["Pick", "Back"] },
-        { "View Set", ["Pick", "Back"] },
-        { "Practice", ["Pick", "Back"] },
-        { "View Scores", ["Pick", "Back"] }
+        { "Delete Set", pickMenuList },
+        { "Delete Element From Set", pickMenuList },
+        { "View Set", pickMenuList },
+        { "Practice", pickMenuList },
+        { "View Scores", pickMenuList }
     };
     private string lastKey;
     private string curKey;
-    private List<string> curList;
 
     public Menu()
     {
-        curKey = MenuTypes.ElementAt(0);
+        curKey = "Main";
         p = new Panel(curKey);
-        List<string>? temp = commands.GetValueOrDefault(curKey);
-        curList = temp == null ? new List<string>() : temp;
-        lastKey = SetLast();
+        lastKey = "";
     }
 
     public void ExecCommand(int n)
     {
-        string command = curList.ElementAt(n);
-        switch(command)
+        string command = CurList().ElementAt(n);
+        if(command.Equals("Back")) {
+            curKey = lastKey;
+            lastKey = "Main";
+        } else if(command.Equals("Pick")) {
+            lastKey = "";
+            Pick();
+        } else {
+            lastKey = curKey;
+            curKey = command;
+        }
+        Console.WriteLine("Executing command " + n);
+    }
+
+    public int GetExitVal()
+    {
+        if(curKey.Equals("Main")) return CurList().Count + 1;
+        else return 0;
+    }
+
+    public int[] GetCommandRange()
+    {
+        return [1, CurList().Count];
+    }
+
+    private List<string> CurList()
+    {
+        List<string>? temp = commands.GetValueOrDefault(curKey);
+        return temp == null ? new List<string>() : temp;
+    }
+
+    private void Pick()
+    {
+        switch(curKey)
         {
-            case "Add":
-                break;
-            case "Delete":
-                break;
-            case "View Set":
-                break;
-            case "Practice":
-                break;
-            case "View Scores":
-                break;
             case "Add Set":
                 break;
             case "Add Element To Set":
@@ -53,28 +75,12 @@ class Menu
                 break;
             case "Delete Element From Set":
                 break;
-            case "Pick":
+            case "View Set":
                 break;
-            case "Back":
+            case "Practice":
+                break;
+            case "View Scores":
                 break;
         }
-        Console.WriteLine("Executing command " + n);
-    }
-
-    public int GetExitVal()
-    {
-        if(curKey.Equals(MenuTypes.ElementAt(0))) return curList.Count + 1;
-        else return 0;
-    }
-
-    public int[] GetCommandRange()
-    {
-        return [1, curList.Count];
-    }
-
-    private string SetLast()
-    {
-        if(curKey.Equals(MenuTypes.ElementAt(0))) return "";
-        else return curKey;
     }
 }
---
+++ Flashcards/UILogic/PrintInfo.cs
@@ -0,0 +1,6 @@
+namespace UILogic;
+
+class PrintInfo
+{
+    
+}
