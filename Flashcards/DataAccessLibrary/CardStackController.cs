268f6a2f09b1091c7ea8fa55d21db8f0c19ddd98
Adam Polak <adamjpolak@gmail.com>
2024-06-14 13:35:49 -0500

    var name changes for consistency

--- Flashcards/DataAccessLibrary/CardStackController.cs
+++ Flashcards/DataAccessLibrary/CardStackController.cs
@@ -6,40 +6,40 @@ namespace DataAccessLibrary;
 public static class CardStackController
 {
 
-    public static void Insert(string name, ValidConnection valid)
+    public static void Insert(string name, ValidConnection validConnection)
     {
-        if(Contains(name, valid)) return;
-        NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO " + ValidConnection.TableNames.ElementAt(0) + " (name) VALUES (@n);", valid.GetConnection());
+        if(Contains(name, validConnection)) return;
+        NpgsqlCommand cmd = new NpgsqlCommand("INSERT INTO " + ValidConnection.TableNames.ElementAt(0) + " (name) VALUES (@n);", validConnection.GetConnection());
         cmd.Parameters.AddWithValue("n", name);
         cmd.ExecuteNonQuery();
     }
 
-    public static void Update(string old, string name, ValidConnection valid)
+    public static void Update(string old, string name, ValidConnection validConnection)
     {
-        if(!Contains(old, valid)) return;
-        if(Contains(name, valid)) return;
-        NpgsqlCommand cmd = new NpgsqlCommand("UPDATE " + ValidConnection.TableNames.ElementAt(0) + " SET name=@n WHERE name=@o;", valid.GetConnection());
+        if(!Contains(old, validConnection)) return;
+        if(Contains(name, validConnection)) return;
+        NpgsqlCommand cmd = new NpgsqlCommand("UPDATE " + ValidConnection.TableNames.ElementAt(0) + " SET name=@n WHERE name=@o;", validConnection.GetConnection());
         cmd.Parameters.AddWithValue("o", old);
         cmd.Parameters.AddWithValue("n", name);
         cmd.ExecuteNonQuery();
     }
 
-    public static void Delete(string name, ValidConnection valid)
+    public static void Delete(string name, ValidConnection validConnection)
     {
-        if(!Contains(name, valid)) return;
-        NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM " + ValidConnection.TableNames.ElementAt(0) + " WHERE name=@n;", valid.GetConnection());
+        if(!Contains(name, validConnection)) return;
+        NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM " + ValidConnection.TableNames.ElementAt(0) + " WHERE name=@n;", validConnection.GetConnection());
         cmd.Parameters.AddWithValue("n", name);
         cmd.ExecuteNonQuery();
     }
 
-    public static List<CardStack> Read(ValidConnection valid)
+    public static List<CardStack> Read(ValidConnection validConnection)
     {
-        return (List<CardStack>)valid.GetConnection().Query<CardStack>("SELECT * FROM " + ValidConnection.TableNames.ElementAt(0) + ";");
+        return (List<CardStack>)validConnection.GetConnection().Query<CardStack>("SELECT * FROM " + ValidConnection.TableNames.ElementAt(0) + ";");
     }
 
-    public static bool Contains(string name, ValidConnection valid) 
+    public static bool Contains(string name, ValidConnection validConnection) 
     {
-        List<CardStack> list = Read(valid);
+        List<CardStack> list = Read(validConnection);
         foreach(CardStack cardStack in list) if(name.Equals(cardStack.Name)) return true;
         return false;
     }
