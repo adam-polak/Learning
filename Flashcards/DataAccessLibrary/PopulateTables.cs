6a1638ecf1b07ab2ed8d4b949b64efb357694c10
Adam Polak <adamjpolak@gmail.com>
2024-06-14 13:38:05 -0500

    modified populate tables to work with static controllers

--- Flashcards/DataAccessLibrary/PopulateTables.cs
+++ Flashcards/DataAccessLibrary/PopulateTables.cs
@@ -3,9 +3,6 @@ namespace DataAccessLibrary;
 public static class PopulateTables
 {
     private static ValidConnection? connection;
-    private static CardStackController? cardStackController;
-    private static CardController? cardController;
-    private static CardScoreController? cardScoreController;
 
     private static List<string> stack_names = ["Marine Science", "Animal Noises"];
     private static List<string> MarineFrontCard = ["Whales live in the _____", "Beaches have ______", "Turtles lay eggs in the ______"];
@@ -18,48 +15,45 @@ public static class PopulateTables
         connection = valid;
         if(!CardStackController.Contains(stack_names.ElementAt(0), connection)) 
         {
-            cardStackController = new CardStackController(connection);
             PopulateStackTable();
-            cardController = new CardController(connection);
             PopulateCardTable();
-            cardScoreController = new CardScoreController(connection);
             PopulateScoreTable();
         }
     }
 
     private static void PopulateCardTable()
     {
-        if(cardController == null) return;
+        if(connection == null) return;
         string stack_name = stack_names.ElementAt(0);
         Card c;
         for(int i = 0; i < MarineFrontCard.Count(); i++) 
         {
             c = new Card() {Name = stack_name, Front = MarineFrontCard.ElementAt(i), Back = MarineBackCard.ElementAt(i)};
-            cardController.Insert(c);
+            CardController.Insert(c, connection);
         }
         stack_name = stack_names.ElementAt(1);
         for(int i = 0; i < AnimalFrontCard.Count(); i++)
         {
             c = new Card() {Name = stack_name, Front = AnimalFrontCard.ElementAt(i), Back = AnimalBackCard.ElementAt(i)};
-            cardController.Insert(c);
+            CardController.Insert(c, connection);
         }
     }
 
     private static void PopulateScoreTable()
     {
-        if(cardScoreController == null || cardController == null) return;
+        if(connection == null) return;
         CardScore cardScore;
         List<Card> cards;
         Random rnd = new Random();
         foreach(string stack_name in stack_names)
         {
-            cards = cardController.Read(stack_name);
+            cards = CardController.Read(stack_name, connection);
             if(cards != null) 
             {
                 for(int i = 0; i < 5; i++)
                 {
                     cardScore = new CardScore() {Name = stack_name, Date = DateTime.Now.ToShortDateString(), Score = rnd.Next(cards.Count()) + "/" + cards.Count()};
-                    cardScoreController.Insert(cardScore);
+                    CardScoreController.Insert(cardScore, connection);
                 }
             }
         }
@@ -67,7 +61,7 @@ public static class PopulateTables
 
     private static void PopulateStackTable()
     {
-        if(cardStackController == null) return;
-        foreach(string s in stack_names) cardStackController.Insert(s);
+        if(connection == null) return;
+        foreach(string s in stack_names) CardStackController.Insert(s, connection);
     }
 }
