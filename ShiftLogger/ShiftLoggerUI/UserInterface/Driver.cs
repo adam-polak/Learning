5ef4b039c7361dccc938f4ca6edd9831ac42944c
Adam Polak <adamjpolak@gmail.com>
2024-06-21 10:50:46 -0500

    set up all methods for ui

--- ShiftLogger/ShiftLoggerUI/UserInterface/Driver.cs
+++ ShiftLogger/ShiftLoggerUI/UserInterface/Driver.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using ShiftLoggerUI.Controllers;
+using ShiftLoggerUI.Models;
 using Spectre.Console;
 
 namespace ShiftLoggerUI;
@@ -20,27 +21,118 @@ public class Driver
         username = "";
     }
 
-    public void Run()
+    public void LoginScreen()
     {
-
-    }
-
-    private void LoginScreen()
-    {
-        if(loggedIn) return;
+        if(loggedIn) UserScreen();
         string choice = PrintOptions("What would you like to do?", ["Login", "Create User"]);
-        if(choice.Equals("Login")) RunLogin();
-        else RunCreateUser();
+        if(choice.Equals("Login")) Login();
+        else CreateUser();
     }
 
-    private void RunLogin()
+    private void UserScreen()
     {
-        
+        string choice = PrintOptions("What would you like to do?", ["View Shift History", "Start Shift", "End Shift", "Logout"]);
+        switch(choice)
+        {
+            case "View Shift History":
+                List<Shift> shifts = shiftController.ViewShifts(username, session_key);
+                PrintShifts(shifts);
+                break;
+            case "Start Shift":
+                Console.WriteLine(shiftController.StartShift(username, session_key));
+                break;
+            case "End Shift":
+                Console.WriteLine(shiftController.EndShift(username, session_key));
+                break;
+            case "Logout":
+                loggedIn = userController.Logout(username, session_key);
+                LoginScreen();
+                break;
+        }
+        if(loggedIn) 
+        {
+            Console.WriteLine("\n(Press enter to continue)");
+            Console.ReadLine();
+            UserScreen();
+        }
     }
 
-    private void RunCreateUser()
+    private void Login()
     {
+        string user = "";
+        string pass;
+        string exit;
+        int key = -1;
+        while(!loggedIn)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter username: ");
+            user = Console.ReadLine() ?? "";
+            Console.WriteLine("Enter password: ");
+            pass = Console.ReadLine() ?? "";
+            key = userController.Login(user, pass);
+            loggedIn = key != -1;
+            if(!loggedIn)
+            {
+                exit = PrintOptions("Incorrect Login\nWould you like to exit?", ["Yes", "No"]);
+                if(exit.Equals("Yes")) break;
+            }
+        }
+        if(!loggedIn) LoginScreen();
+        else {
+            username = user;
+            session_key = key;
+            UserScreen();
+        }
+    }
 
+    private void PrintShifts(List<Shift> shifts)
+    {
+        Table table = new Table();
+        table.Title($"Shifts for {username}");
+        table.AddColumn("Shift #");
+        table.AddColumn("Start Time");
+        table.AddColumn("End Time");
+        foreach(Shift shift in shifts) table.AddRow("" + shift.Id, shift.Start_Time, shift.End_Time);
+        AnsiConsole.Write(table);
+    }
+
+    private void CreateUser()
+    {
+        bool valid = false;
+        string user;
+        string pass;
+        while(!valid)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter username: ");
+            user = Console.ReadLine() ?? "";
+            if(user.Length  < 2)
+            {
+                Console.WriteLine("Username needs to contain 3 or more characters...");
+                Console.WriteLine("(Press enter to continue)");
+                Console.ReadLine();
+                continue;
+            }
+            Console.WriteLine("Enter your password: ");
+            pass = Console.ReadLine() ?? "";
+            if(pass.Length < 6)
+            {
+                Console.WriteLine("Password needs to contain 6 or more characters...");
+                Console.WriteLine("(Press enter to continue)");
+                Console.ReadLine();
+                continue;   
+            }
+            valid = userController.CreateUser(user, pass);
+            if(!valid)
+            {
+                Console.WriteLine("Username already exists, use a different one...");
+                Console.WriteLine("(Press enter to continue)");
+                Console.ReadLine();
+                continue;   
+            }
+        }
+        LoginScreen();
     }
 
     private string PrintOptions(string title, List<string> commands)
