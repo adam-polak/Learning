3bab70bfd5a2af1c72d4c47a0f93b790d8ffafb5
Adam Polak <adamjpolak@gmail.com>
2024-06-24 14:25:03 -0500

    formatting answer

--- SportWebCrawler/Program.cs
+++ SportWebCrawler/Program.cs
@@ -1,14 +1,22 @@
 ﻿using SportWebCrawler.Controllers;
 using SportWebCrawler.Models;
-
+string dash = "--------------------------";
 Console.WriteLine("Enter the date you would like to check for games played: (Format as mm/dd/yyyy)");
 WebController webController = new WebController();
-List<Game>? games = webController.GetGamesOnDate(Console.ReadLine() ?? "");
+string date = Console.ReadLine() ?? "";
+List<Game>? games = webController.GetGamesOnDate(date);
 if(games != null)
 {
+    Console.WriteLine($"\n{dash}");
+    Console.WriteLine("Games played on " + date);
+    Console.WriteLine(dash);
     if(games.Count == 0) Console.WriteLine("There was no games played on this day!");
-    foreach(Game game in games)
-    {
-        Console.WriteLine($"{game.Date} \n\t{game.Winner}: {game.Winner_Score}\n\t{game.Loser}: {game.Loser_Score}");
+    else {
+        Console.WriteLine("\n");
+        foreach(Game game in games)
+        {
+            Console.WriteLine($"\t{game.Winner}: {game.Winner_Score}\n\t{game.Loser}: {game.Loser_Score}\n");
+        }
+        Console.WriteLine(dash);
     }
 } else Console.WriteLine("Invalid date format...");
