5d2c4c1b5bee41eeb49021ff68c25c338ae31312
Adam Polak <adamjpolak@gmail.com>
2024-06-25 21:35:38 -0500

    update page working

--- WaterDrinkingLog/Pages/Update.cshtml.cs
+++ WaterDrinkingLog/Pages/Update.cshtml.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Npgsql;
@@ -27,12 +28,31 @@ namespace WaterDrinkingLog.Pages
 
         private DrinkingWaterModel GetById(int id)
         {
-            return new DrinkingWaterModel();
+            using(NpgsqlConnection connection = new NpgsqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                connection.Open();
+                DrinkingWaterModel? ans = connection.Query<DrinkingWaterModel>($"SELECT * FROM drinking_water WHERE id={id};").FirstOrDefault();
+                connection.Close();
+                return ans ?? new DrinkingWaterModel();
+            }
         }
 
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid) return Page();
+            
+            using(NpgsqlConnection connection = new NpgsqlConnection(_configuration.GetConnectionString("Default")))
+            {
+                connection.Open();
+                string shortDate = DrinkingWater.Date.ToShortDateString();
+                string sqlCommand = "UPDATE drinking_water"
+                                    + $" SET date='{shortDate}', quantity={DrinkingWater.Quantity}"
+                                    + $" WHERE id={DrinkingWater.Id};";
+                NpgsqlCommand cmd = new NpgsqlCommand(sqlCommand, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
             return RedirectToPage("./Index");
         }
     }
