877b65b88af00378e62c235da612a93bc18d7456
Adam Polak <adamjpolak@gmail.com>
2024-06-27 16:31:23 -0500

    added error message when trying to add movie that already exists

--- SuperMovies/Pages/Create.cshtml.cs
+++ SuperMovies/Pages/Create.cshtml.cs
@@ -31,15 +31,33 @@ public class CreateModel : PageModel
         using(NpgsqlConnection connection = new NpgsqlConnection(_configuration.GetConnectionString("Default")))
         {
             connection.Open();
-            AddMovie.Id = SetId(connection);
-            string sqlCommand = "INSERT INTO movie_table (id, title, description, length_minutes)"
-                                + $" VALUES ({AddMovie.Id}, '{AddMovie.Title}', '{AddMovie.Description}', {AddMovie.Length_Minutes})";
-            NpgsqlCommand cmd = new NpgsqlCommand(sqlCommand, connection);
-            cmd.ExecuteNonQuery();
-            connection.Close();
+            if(!ContainsTitle(connection))
+            {
+                AddMovie.Id = SetId(connection);
+                string sqlCommand = "INSERT INTO movie_table (id, title, description, length_minutes)"
+                                    + $" VALUES ({AddMovie.Id}, '{AddMovie.Title}', '{AddMovie.Description}', {AddMovie.Length_Minutes})";
+                NpgsqlCommand cmd = new NpgsqlCommand(sqlCommand, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                return RedirectToPage("./Index");
+            } else {
+                connection.Close();
+                RedirectToPageResult index = RedirectToPage("./Index");
+                index.RouteValues = new RouteValueDictionary() {{"errorMessage", $"exists-{AddMovie.Id}" }};
+                return index;
+            }
         }
+    }
 
-        return RedirectToPage("./Index");
+    private bool ContainsTitle(NpgsqlConnection connection)
+    {
+        List<Movie> movies = (List<Movie>)connection.Query<Movie>($"SELECT * FROM movie_table WHERE title='{AddMovie.Title}';");
+        if(movies.Count > 0)
+        {
+            AddMovie.Id = movies.ElementAt(0).Id;
+            return true;
+        }
+        return false;
     }
 
     private int SetId(NpgsqlConnection connection)
--- SuperMovies/Pages/Index.cshtml.cs
+++ SuperMovies/Pages/Index.cshtml.cs
@@ -12,6 +12,7 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     [BindProperty(SupportsGet = true)]
     public string? SearchFor { get; set; }
+    public string? ErrorMessage;
     public List<Movie> Movies;
     private List<Movie> movieList;
 
@@ -34,8 +35,10 @@ public class IndexModel : PageModel
         }
     }
 
-    public void OnGet()
+    public void OnGet(string? errorMessage)
     {
+        ErrorMessage = errorMessage;
+        if(ErrorMessage != null) ErrorMessage = SetErrorMessage();
         if(!string.IsNullOrEmpty(SearchFor))
         {
             Movies = new List<Movie>();
@@ -45,4 +48,47 @@ public class IndexModel : PageModel
             }
         } else Movies = movieList;
     }
+
+    private Movie? GetMovieById(int id)
+    {
+        using(NpgsqlConnection connection = new NpgsqlConnection(_configuration.GetConnectionString("Default")))
+        {
+            connection.Open();
+            List<Movie> movies = (List<Movie>)connection.Query<Movie>($"SELECT * FROM movie_table WHERE id={id};");
+            connection.Close();
+            if(movies.Count == 0) return null;
+            return movies.ElementAt(0);
+        }
+    }
+
+    private string? SetErrorMessage()
+    {
+        if(ErrorMessage == null) return null;
+        bool firstPart = true;
+        string error = "";
+        string number = "";
+        char[] arr = ErrorMessage.ToCharArray();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(firstPart && arr[i] != '-') error += arr[i];
+            else {
+                firstPart = false;
+                if(arr[i] != '-') number += arr[i];
+            }
+        }
+        int id;
+        if(Int32.TryParse(number, out id))
+        {
+            Movie? movie = GetMovieById(id);
+            if(movie == null) return null;
+            switch(error)
+            {
+                case "exists":
+                    return $"Couldn't add \"{movie.Title}\" because it already exists";
+                default:
+                    return null;
+            }
+        } else return null;
+    }
+
 }
