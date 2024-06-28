82ec75ea69c5dc5bd8edc60f43d4293cc96ddf1d
Adam Polak <adamjpolak@gmail.com>
2024-06-28 15:37:28 -0500

    set up controller

--- TodoSPA/TodoAPI/Controllers/TodoController.cs
+++ TodoSPA/TodoAPI/Controllers/TodoController.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using TodoAPI.DataAccess;
+
 namespace TodoAPI.Controllers;
 
-public class TodoController
+[ApiController]
+[Route("todoapi")]
+public class TodoController : ControllerBase
 {
-    
+    TodoAccess todoAccess;
+
+    public TodoController()
+    {
+        todoAccess = new TodoAccess();
+    }
+
+    [HttpPost("add")]
+    public IActionResult AddTodo([FromBody] string description)
+    {
+        return Ok();
+    }
+
+    [HttpPut("update/{id}")]
+    public IActionResult UpdateTodo(int id, [FromBody] string description)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("delete/{id}")]
+    public IActionResult DeleteTodo(int id)
+    {
+        return Ok();
+    }
 }
