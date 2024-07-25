72968b42b828f8fcd2584a6212e972fc148c9fd8
Adam Polak <adamjpolak@gmail.com>
2024-07-24 19:17:20 -0500

    custom font file

--- LearnReactSPA/Frontend/nextjs-dashboard/app/page.tsx
+++ LearnReactSPA/Frontend/nextjs-dashboard/app/page.tsx
@@ -1,10 +1,12 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <div className={styles.shape} />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
@@ -28,6 +30,9 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div
+        className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+      />
     </main>
   );
 }
